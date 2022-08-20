600cb04c3729c9d6e5a01c49254946c4963571fb
mcleolee <1772133019@qq.com>
2022-08-20 16:34:14 +0800

    8.20+1634

--- DEMO/src/File.cpp
+++ DEMO/src/File.cpp
@@ -1,6 +1,30 @@
 #include "headerFile.h"
+#include <iostream>
 
-int demo::perimeter(int a,int b)
+using namespace std;
+
+void sqlist::show_msg()
+    {
+        cout<< "len="<<len<<" size="<<size<<" addr="<<addr<<endl;
+    }
+
+sqlist::sqlist(int size, int *arr, int len)
 {
-    return 2*(a+b);
+    addr = new int[size];
+    this->len = len;
+    this->size = size;
+    for(int i=0; i<len; i++)
+    {
+        addr[i] = arr[i];
+    }
 }
+
+sqlist::sqlist(const sqlist &obj)
+{
+    // deep copy
+    addr = new int[obj.size];
+    
+}
+
+
+
--- DEMO/src/headerFile.h
+++ DEMO/src/headerFile.h
@@ -1,11 +1,29 @@
 #ifndef _DEMO_H
 #define _DEMO_H
 
-class demo{
-// private:
-//     int a,b;
+const int N = 100;
+
+class sqlist
+{
+private:
+    int *addr;
+    int len;
+    int size;
+
 public:
-    int perimeter(int a,int b); 
+    sqlist(int size = N, int *arr = nullptr, int len = 0);
+    sqlist(const sqlist &obj);
+    sqlist(sqlist &&obj);
+    ~sqlist();
+
+    bool insert(int pos, int data);
+    bool insert(int pos, int *arr, int len);
+    bool show();
+    bool del(int pos);
+    bool change(int pos, int data);
+    int search(int pos);
+
+    void show_msg();
 };
 
 #endif
